7d72c0c28658f374696ac27cf708df03f68ffb9d
Andres Jurado anmijurane <anmijurane@gmail.com>
2021-07-04 10:53:10 -0500

    estilos sass

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import DailyGoalsApp from './DailyGoalsApp'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <DailyGoalsApp />,
   document.getElementById('root')
 )
