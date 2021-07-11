25a3313a4d17f82f71f5b00089fafcd3ae1a7714
Andres Jurado anmijurane <anmijurane@gmail.com>
2021-07-11 15:38:53 -0500

    componente centrado y corrregido la jerarquia de tags

--- src/components/dailyGoals/NothingSelected.jsx
+++ src/components/dailyGoals/NothingSelected.jsx
@@ -3,21 +3,19 @@ import { GrScheduleNew } from 'react-icons/gr';
 
 export function NothingSelected() {
   return (
-    <div className='nothing__main-content'>
-      <p>
+    <section className='nothing__main-content'>
+      <div className='notging__actions'>
         Select something
 
         <div className="or__option">
           <hr /> <span>Or</span> <hr />
         </div>
 
-
-        <button className="box is-flex-center is-flex-direc-column">
+        <button className="box is-flex-center is-flex-direc-column is-clickable">
           <GrScheduleNew className='fz-3' />
           <h3 className='mt-3'> Create New Entry </h3>
         </button>
-
-      </p>
-    </div>
+      </div>
+    </section>
   )
 }
