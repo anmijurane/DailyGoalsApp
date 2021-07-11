30a8e7cdbc072063336a3ab3a3c13bf2641d78fc
Andres Jurado anmijurane <anmijurane@gmail.com>
2021-07-11 14:25:42 -0500

    name del auth en Ridux y accion logout

--- src/components/dailyGoals/Sidebar.jsx
+++ src/components/dailyGoals/Sidebar.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { FaUserCircle, FaCalendarPlus } from 'react-icons/fa';
 import { DailyEntries } from './DailyEntries';
+import { useDispatch, useSelector } from 'react-redux';
+import { logoutAsync } from '../../actions/auth';
 
 export default function Sidebar() {
+  const dispatch = useDispatch();
+  const { name } = useSelector(state => state.auth);
+
+  const handleLogout = () => {
+    dispatch( logoutAsync() );
+  }
+
   return (
     <aside className='daily__sidebar'>
       <nav className="daily__sidebar-navbar">
@@ -11,10 +20,13 @@ export default function Sidebar() {
         <div className="daily__sidebar-header">
           <div className="is-flex-center">
             <FaUserCircle />
-            <span className='ml-2'>Andres Jurado</span>
+            <span className='ml-2'>{ name }</span>
           </div>
-          <button>
-            <GiHamburgerMenu />
+          <button
+            onClick={handleLogout}
+          >
+            LogOut
+            {/* <GiHamburgerMenu /> */}
           </button>
         </div>
       </nav>
